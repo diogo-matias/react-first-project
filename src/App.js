78c65a0d4ed9018eabcdc5aa438d5a37cf006f43
diogo-pc <diogomaccedo200@gmail.com>
2022-05-16 17:11:09 -0300

    aprendendo principios básicos React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+import Titulo from "./components/Titulo";
+
+const App = () => {
+  let [tasks, setTasks] = useState("teste");
+  let [titulo] = useState("Amanheceu ");
+
+  function clickButton() {
+    if (tasks === "teste") {
+      setTasks("TESTE");
+    } else {
+      setTasks("teste");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Titulo titulo={titulo} />
+      <h1> {tasks} </h1>
+
+      <button onClick={clickButton}>BTN1</button>
     </div>
   );
-}
+};
 
 export default App;
